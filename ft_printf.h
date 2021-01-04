e1fe6f992d6f7add09163e2c4ac2a445b021b0b0
Kyllian <kdelport@student.42lyon.fr>
2021-01-04 12:16:33 +0100

    .

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: kdelport <kdelport@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 12:40:28 by kdelport          #+#    #+#             */
-/*   Updated: 2021/01/04 11:51:27 by kdelport         ###   ########lyon.fr   */
+/*   Updated: 2021/01/04 12:16:14 by kdelport         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ void			ft_putchar(char c, int *count);
 void			ft_putnbr(int n, int *count);
 void			ft_unsigned_putnbr(unsigned long long n, int *count);
 void			ft_putstr(char *str, int *count);
+void			fill_space(char c, int size, int *count);
 void			ft_itoh(unsigned long long nb, int low, int *ct, t_flags *flg);
 int				nbr_length(int nbr);
 int				u_nbr_len(unsigned long long nbr, unsigned int size_base);
@@ -43,6 +44,7 @@ int				ft_strlen(const char *s);
 
 int				is_conversions(char c);
 void			struct_initialize(t_flags *flags);
+void			check_space_is_neg(t_flags *flags);
 void			to_decimal(va_list list, int *count, t_flags *flags);
 void			to_unsigned_decimal(va_list list, int *count, t_flags *flags);
 void			to_hexa(va_list list, int *count, int is_min, t_flags *flags);
@@ -51,7 +53,6 @@ void			to_pointer_address(va_list list, int *count, t_flags *flags);
 void			to_string(va_list list, int *count, t_flags flags);
 void			to_percent(int *count, t_flags *flags);
 
-void			fill_space(char c, int size, int *count);
 
 void			ope_dot_address(t_flags *f, int *count, int arg, int *len);
 void			ope_dot(t_flags *f, int *count, int arg, int *len);
@@ -63,7 +64,5 @@ void			operands_string_dot(t_flags flags, int *count, int len);
 void			ope_space_string(t_flags flags, int *count, int len);
 void			ope_space_string_suff(t_flags flags, int *count, int len);
 void			print_string(t_flags flags, int *count, char *str, int len);
-
-void			check_space_is_neg(t_flags *flags);
 
 #endif
